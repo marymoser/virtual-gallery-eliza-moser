558ea5dae62313e7ae95bd082688959325212f68
marymoser <mmoser@umass.edu>
2017-04-09 23:14:58 -0400

    Update Muse and Olive Tree images. Dynamically-generated modal text

--- script/createmap.js
+++ script/createmap.js
@@ -10,42 +10,51 @@ var stageElements = {
 		{
 			Coords: "531,51,610,144",
 			Title: "Mel",
-			ImgLink: "img/works/Mel.jpg"
+			ImgLink: "img/works/Mel.jpg",
+			Medium: "Charcoal on Paper"
 		},
 		{
 			Coords: "526,148,613,257",
 			Title: "Cast of Michaelangelo",
-			ImgLink: "img/works/Michaelangelo.jpg"
+			ImgLink: "img/works/Michaelangelo.jpg",
+			Medium: "Charcoal on Paper"
 		},
 		{
       Coords: "757,133,844,278",
-			Title: "Falco II (figure study)",
-			ImgLink: "img/works/Falco_2.jpg"
+			Title: "Falco II",
+			ImgLink: "img/works/Falco_2.jpg",
+			Medium: "Oil on Canvas",
+			Notes: "figure study"
     },
 		{
 			Coords: "1005,49,1080,139",
 			Title: "Portrait of the Artist, Age 21",
-			ImgLink: "img/works/Self_Portrait_Age_21.jpg"
+			ImgLink: "img/works/Self_Portrait_Age_21.jpg",
+			Medium: "Oil on Canvas"
 		},
 		{
 			Coords: "989,144,1096,238",
 			Title: "Terracotta and Green",
-			ImgLink: "img/works/Terracotta_and_Green.jpg"
+			ImgLink: "img/works/Terracotta_and_Green.jpg",
+			Medium: "Oil on Canvas"
 		},
 		{
 			Coords: "1224,155,1307,279",
 			Title: "Resting Figure",
-			ImgLink: "img/works/Resting_Figure.jpg"
+			ImgLink: "img/works/Resting_Figure.jpg",
+			Medium: "Oil on Canvas"
 		},
 		{
 			Coords: "1447,40,1534,148",
 			Title: "Cast of Saint Theresa",
-			ImgLink: "img/works/Saint_Theresa.jpg"
+			ImgLink: "img/works/Saint_Theresa.jpg",
+			Medium: "Charcoal on Paper"
 		},
 		{
 			Coords: "1451,151,1530,254",
 			Title: "Portrait of Jothi",
-			ImgLink: "img/works/Jothi.jpg"
+			ImgLink: "img/works/Jothi.jpg",
+			Medium: "Charcoal on Paper"
 		}]
 }
 
@@ -57,74 +66,90 @@ var eastElements = {
 		{
 			Coords: "118,195,216,392",
 			Title: "Shy Figure",
-			ImgLink: "img/works/Shy_Figure.jpg"
+			ImgLink: "img/works/Shy_Figure.jpg",
+			Medium: "Oil on Canvas"
 		},
 		{
 			Coords: "263,203,346,298",
 			Title: "Portrait of an Italian Girl",
-			ImgLink: "img/works/Italian_Girl.jpg"
+			ImgLink: "img/works/Italian_Girl.jpg",
+			Medium: "Oil on Canvas"
 		},
 		// Left red panel
 		{
 			Coords: "507,120,582,217",
 			Title: "The Muse",
-			ImgLink: "img/works/Muse.jpg"
+			ImgLink: "img/works/Muse.jpg",
+			Medium: "Oil on Canvas"
 		},
 		{
 			Coords: "503,221,585,340",
-			Title: "Falco (figure study)",
-			ImgLink: "img/works/Falco_1.jpg"
+			Title: "Falco",
+			ImgLink: "img/works/Falco_1.jpg",
+			Medium: "Charcoal on Paper",
+			Notes: "figure study"
 		},
 		// Dawn
 		{
 			Coords: "698,178,837,330",
 			Title: "Dawn",
-			ImgLink: "img/works/Dawn.jpg"
+			ImgLink: "img/works/Dawn.jpg",
+			Medium: "Oil on Canvas"
 		},
 		// Right red panel
 		{
 			Coords: "965,120,1030,203",
 			Title: "Portrait of Elena",
-			ImgLink: "img/works/Elena.jpg"
+			ImgLink: "img/works/Elena.jpg",
+			Medium: "Oil on Canvas"
 		},
 		{
 			Coords: "960,206,1037,306",
 			Title: "Alessio",
-			ImgLink: "img/works/Alessio.jpg"
+			ImgLink: "img/works/Alessio.jpg",
+			Medium: "Charcoal on Paper"
 		},
 		// Right standing easel
 		{
 			Coords: "1103,186,1212,383",
 			Title: "Seated Figure",
-			ImgLink: "img/works/Seated_Figure.jpg"
+			ImgLink: "img/works/Seated_Figure.jpg",
+			Medium: "Charcoal on Paper"
 		},
 		// Right Small Round Table
 		{
 			Coords: "1258,224,1333,340",
-			Title: "Echo (work-in-progress)",
-			ImgLink: "img/works/Echo_WIP.jpg"
+			Title: "Echo",
+			ImgLink: "img/works/Echo_WIP.jpg",
+			Medium: "Oil on Panel",
+			Notes: "work-in-progress"
 		},
 		// Right Large Table
 		{
 			Coords: "1523,199,1614,325",
-			Title: "Djole, or Portrait of Montenegrin Boy",
-			ImgLink: "img/works/Montenegrin_Boy.jpg"
+			Title: "Djole",
+			ImgLink: "img/works/Montenegrin_Boy.jpg",
+			Medium: "Charcoal on Paper",
+			AltTitle: "Portrait of Montenegrin Boy"
 		},
 		{
 			Coords: "1617,278,1686,378",
 			Title: "Olive Tree",
-			ImgLink: "img/works/Olive_Tree_Pencil.jpg"
-		},
-		{
+			ImgLink: "img/works/Olive_Tree_Pencil.jpg",
+			Medium: "Pencil on Paper"
+		}
+		/*{
 			Coords: "1730,206,1809,320",
 			Title: "Untitled (figure study)",
-			ImgLink: "img/works/Figure_Study_1.jpg"
+			ImgLink: "img/works/Figure_Study_1.jpg",
+			Medium: "Oil on Canvas"
 		},
 		{
 			Coords: "1816,254,1897,352",
 			Title: "Untitled (figure study)",
-			ImgLink: "img/works/Figure_Study_2.jpg"
-		}]
+			ImgLink: "img/works/Figure_Study_2.jpg",
+			Medium: "Charcoal on Paper"
+		}*/]
 }
 
 var westElements = {
@@ -135,47 +160,56 @@ var westElements = {
 		{
 			Coords: "166,124,308,302",
 			Title: "Grandpa",
-			ImgLink: "img/works/Grandpa.JPG"
+			ImgLink: "img/works/Grandpa.JPG",
+			Medium: "Charcoal on Paper"
 		},
 		// Left table
 		{
 			Coords: "364,213,448,315",
 			Title: "Cinderella",
-			ImgLink: "img/works/Cinderella.jpg"
+			ImgLink: "img/works/Cinderella.jpg",
+			Medium: "Oil on Panel"
 		},
 		// Gray panel, left side
 		{
 			Coords: "498,178,592,304",
 			Title: "Portrait of Marco",
-			ImgLink: "img/works/Marco.jpg"
+			ImgLink: "img/works/Marco.jpg",
+			Medium: "Charcoal on Paper",
+			Notes: "unfinished"
 		},
 		// Gray panel, middle
 		{
 			Coords: "600,168,718,270",
 			Title: "November Still Life",
-			ImgLink: "img/works/November_Still_Life.jpg"
+			ImgLink: "img/works/November_Still_Life.jpg",
+			Medium: "Oil on Canvas"
 		},
 		{
 			Coords: "602,272,658,328",
 			Title: "Gold and Blue",
-			ImgLink: "img/works/Gold_and_Blue_WIP.jpg"
+			ImgLink: "img/works/Gold_and_Blue_WIP.jpg",
+			Medium: "Oil on Canvas"
 		},
 		{
 			Coords: "663,272,721,328",
 			Title: "Highlights",
-			ImgLink: "img/works/Highlights.jpg"
+			ImgLink: "img/works/Highlights.jpg",
+			Medium: "Oil on Canvas"
 		},
 		// Gray panel, right side
 		{
 			Coords: "727,156,794,349",
 			Title: "Standing Figure",
-			ImgLink: "img/works/Standing_Figure.jpg"
+			ImgLink: "img/works/Standing_Figure.jpg",
+			Medium: "Oil on Canvas"
 		},
 		// Right table
 		{
 			Coords: "830,205,915,317",
 			Title: "The Artist's Boot",
-			ImgLink: "img/works/Artist_Boot.jpg"
+			ImgLink: "img/works/Artist_Boot.jpg",
+			Medium: "Oil on Panel"
 		}
 	]
 }
@@ -283,8 +317,28 @@ function showModal(areaElement) {
 	imgTag.setAttribute("title", artwork.Title);
 
 	// Set modal text
-	document.getElementById("art-modal-text").innerHTML = artwork.Title;
+	document.getElementById("art-modal-text").innerHTML = getModalText(i);
 
 	// Display modal
 	document.getElementById('art-modal').style.display='block';
+}
+
+function getModalText(index) {
+	artwork = currentPage.map[index];
+
+	// Set up title
+	modalText = '<em>' + artwork.Title + '</em>';
+
+	// Check for alternate titles and Notes
+	if(artwork.hasOwnProperty('AltTitle')) {
+		modalText += ", or " + '<em>' + artwork.AltTitle + '</em>';
+	}
+	if(artwork.hasOwnProperty('Notes')) {
+		modalText += " (" + artwork.Notes + ")";
+	}
+
+	//Display medium
+	modalText += ", " + artwork.Medium;
+
+	return modalText;
 }
